8802c3aaa8022949beba04bb26e3c1074dae5328
ChrisWenzlick <23247122+ChrisWenzlick@users.noreply.github.com>
2026-05-10 22:12:12 -0500

    test(domain): add success and failure tests for Result type

---
+++ src/HL7Parser.Domain/Common/Result.cs
@@ -0,0 +1,48 @@
+﻿// <copyright file="Result.cs" company="Christopher Wenzlick">
+// Copyright (c) Christopher Wenzlick. All rights reserved.
+// </copyright>
+
+using System;
+
+namespace HL7Parser.Domain.Common
+{
+    /// <summary>
+    /// Represents the outcome of an operation.
+    /// </summary>
+    /// <typeparam name="T">The value type.</typeparam>
+    public sealed class Result<T>
+    {
+        private Result(bool isSuccess) => this.IsSuccess = isSuccess;
+
+        /// <summary>
+        /// Gets a value indicating whether the operation succeeded.
+        /// </summary>
+        public bool IsSuccess { get; }
+
+        /// <summary>
+        /// Gets the value.
+        /// </summary>
+        public T Value => throw new NotImplementedException();
+
+        /// <summary>
+        /// Gets the error.
+        /// </summary>
+        public string Error { get; } = string.Empty;
+
+        /// <summary>
+        /// Creates a successful result.
+        /// </summary>
+        /// <param name="value">The value of the <see cref="Result{T}"/> object.</param>
+        /// <returns>A successful <see cref="Result{T}"/> object.</returns>
+        /// <exception cref="NotImplementedException">This method has not been implemented yet.</exception>
+        public static Result<T> Success(T value) => throw new NotImplementedException();
+
+        /// <summary>
+        /// Creates a failed result.
+        /// </summary>
+        /// <param name="error">A <see cref="string"/> explaining the cause of the failure.</param>
+        /// <returns>A failed <see cref="Result{T}"/> object.</returns>
+        /// <exception cref="NotImplementedException">This method has not been implemented yet.</exception>
+        public static Result<T> Failure(string error) => throw new NotImplementedException();
+    }
+}
---
+++ tests/HL7Parser.Tests.Unit/Domain/Common/ResultTests.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Christopher Wenzlick. All rights reserved.
+
+using HL7Parser.Domain.Common;
+
+namespace HL7Parser.Tests.Unit.Domain.Common
+{
+    public class ResultTests
+    {
+        [Fact]
+        public void Success_IsSuccess_ReturnsTrue()
+        {
+            var result = Result<string>.Success("test value");
+
+            Assert.True(result.IsSuccess);
+        }
+
+        [Fact]
+        public void Failure_IsSuccess_ReturnsTrue()
+        {
+            var result = Result<string>.Failure("something went wrong");
+
+            Assert.False(result.IsSuccess);
+        }
+    }
+}
