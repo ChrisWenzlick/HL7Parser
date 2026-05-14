2256915a80b0e104b08ed08b01dff68b8f68ab7b
ChrisWenzlick <23247122+ChrisWenzlick@users.noreply.github.com>
2026-05-13 21:01:57 -0500

    chore(style): expand editorconfig with modern .NET conventions and StyleCop overrides

--- tests/HL7Parser.Tests.Unit/Domain/Common/ResultTests.cs
+++ tests/HL7Parser.Tests.Unit/Domain/Common/ResultTests.cs
@@ -2,40 +2,39 @@
 
 using HL7Parser.Domain.Common;
 
-namespace HL7Parser.Tests.Unit.Domain.Common
+namespace HL7Parser.Tests.Unit.Domain.Common;
+
+public class ResultTests
 {
-    public class ResultTests
+    [Fact]
+    public void Success_IsSuccess_ReturnsTrue()
     {
-        [Fact]
-        public void Success_IsSuccess_ReturnsTrue()
-        {
-            var result = Result<string>.Success("test value");
+        var result = Result<string>.Success("test value");
 
-            Assert.True(result.IsSuccess);
-        }
+        Assert.True(result.IsSuccess);
+    }
 
-        [Fact]
-        public void Failure_IsSuccess_ReturnsTrue()
-        {
-            var result = Result<string>.Failure("something went wrong");
+    [Fact]
+    public void Failure_IsSuccess_ReturnsTrue()
+    {
+        var result = Result<string>.Failure("something went wrong");
 
-            Assert.False(result.IsSuccess);
-        }
+        Assert.False(result.IsSuccess);
+    }
 
-        [Fact]
-        public void Success_Value_ReturnsExpectedValue()
-        {
-            var result = Result<string>.Success("test value");
+    [Fact]
+    public void Success_Value_ReturnsExpectedValue()
+    {
+        var result = Result<string>.Success("test value");
 
-            Assert.Equal("test value", result.Value);
-        }
+        Assert.Equal("test value", result.Value);
+    }
 
-        [Fact]
-        public void Failure_Value_ThrowsInvalidOperationException()
-        {
-            var result = Result<string>.Failure("something went wrong");
+    [Fact]
+    public void Failure_Value_ThrowsInvalidOperationException()
+    {
+        var result = Result<string>.Failure("something went wrong");
 
-            Assert.Throws<InvalidOperationException>(() => result.Value);
-        }
+        Assert.Throws<InvalidOperationException>(() => result.Value);
     }
 }
