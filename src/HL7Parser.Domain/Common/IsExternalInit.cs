f8a7ebe602f66686f40af5ae8b84b6958642b7ca
ChrisWenzlick <23247122+ChrisWenzlick@users.noreply.github.com>
2026-05-21 13:57:51 -0500

    feat(subcomponent): implement Subcomponent value object

---
+++ src/HL7Parser.Domain/Common/IsExternalInit.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="IsExternalInit.cs" company="Christopher Wenzlick">
+// Copyright (c) Christopher Wenzlick. All rights reserved.
+// </copyright>
+//
+// This declaration is required to use init-only properties and record types
+// when targeting netstandard2.0 or .NET Framework. The type exists in the
+// .NET 5+ runtime but must be provided manually for older targets.
+// See: https://developercommunity.visualstudio.com/t/error-cs0518-isexternalinit/1244809
+
+namespace System.Runtime.CompilerServices;
+
+/// <summary>
+/// Reserved for use by the compiler.
+/// </summary>
+/// <remarks>
+/// This type is required by the C# compiler to support init-only properties
+/// and record types on targets older than .NET 5. It is not intended for
+/// direct use in application code.
+/// </remarks>
+internal static class IsExternalInit
+{
+}
---
+++ src/HL7Parser.Domain/Subcomponent.cs
@@ -0,0 +1,57 @@
+﻿// <copyright file="Subcomponent.cs" company="Christopher Wenzlick">
+// Copyright (c) Christopher Wenzlick. All rights reserved.
+// </copyright>
+
+using HL7Parser.Domain.Common;
+
+namespace HL7Parser.Domain;
+
+/// <summary>
+/// Represents the most granular unit of data in an HL7 v2 message.
+/// A subcomponent contains a raw string value and guarantees that value
+/// does not contain HL7 delimiter characters.
+/// </summary>
+public sealed record Subcomponent
+{
+    private static readonly char[] Hl7Delimiters = { '|', '^', '&', '~', '\\' };
+
+    /// <summary>
+    /// Gets the raw text of the <see cref="Subcomponent"/> with no
+    /// parsing or formatting applied.
+    /// </summary>
+    public string RawValue { get; }
+
+    private Subcomponent(string rawValue)
+    {
+        RawValue = rawValue;
+    }
+
+    /// <summary>
+    /// Creates a new <see cref="Subcomponent"/> with the specified value.
+    /// </summary>
+    /// <param name="rawValue">
+    /// The raw string value of the <see cref="Subcomponent"/> with no parsing
+    /// or formatting applied.
+    /// </param>
+    /// <returns>
+    /// A successful <see cref="Result{T}"/> containing the subcomponent,
+    /// or a failed <see cref="Result{T}"/> if the value is invalid.
+    /// </returns>
+    public static Result<Subcomponent> Create(string? rawValue)
+    {
+        if (rawValue is null)
+        {
+            return Result<Subcomponent>.Failure($"Subcomponent parameter {nameof(rawValue)} cannot be null.");
+        }
+
+        var delimiterIndex = rawValue.IndexOfAny(Hl7Delimiters);
+        if (delimiterIndex != -1)
+        {
+            var offendingCharacter = rawValue[delimiterIndex];
+            var error = $"Subcomponent value cannot contain '{offendingCharacter}' as it is a reserved character.";
+            return Result<Subcomponent>.Failure(error);
+        }
+
+        return Result<Subcomponent>.Success(new Subcomponent(rawValue));
+    }
+}
